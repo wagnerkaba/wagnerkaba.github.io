a40e893a8e002a5f986f78e3c1a0b154a5496be0
wagnerkaba <bom.link+github@gmail.com>
2022-02-28 16:29:57 -0300

    criação do formulario e validação em js

--- formulario/main.js
+++ formulario/main.js
@@ -1,41 +1,86 @@
-    var email = document.forms['formulario']['email'];
-    var senha = document.forms['formulario']['senha'];
-    
-    var erro_email = document.getElementById('erro_email');
-    var erro_senha = document.getElementById('erro_senha');
-    
-    email.addEventListener('textInput', verificaEmail);
-    senha.addEventListener('textInput', verificaSenha);
-    
-    function validaAoEnviar(){
-        if (email.value.length < 9) {
-            email.style.border = "1px solid red";
-            erro_email.style.display = "block";
-            email.focus();
-            return false;
-        }
-        if (senha.value.length < 6) {
-            senha.style.border = "1px solid red";
-            erro_senha.style.display = "block";
-            senha.focus();
-            return false;
-        }
-    
+let inputNome = document.forms['formulario']['nome'];
+let inputEmail = document.forms['formulario']['email'];
+let inputTelefone = document.forms['formulario']['telefone'];
+let inputSenha = document.forms['formulario']['senha'];
+
+let containerErroNome = document.getElementById('erro_nome');
+let containerErroEmail = document.getElementById('erro_email');
+let containerErroTelefone = document.getElementById('erro_telefone');
+let containerErroSenha = document.getElementById('erro_senha');
+
+
+inputNome.addEventListener('input', verificaNome);
+inputEmail.addEventListener('input', verificaEmail);
+inputTelefone.addEventListener('input', verificaTelefone);
+inputSenha.addEventListener('input', verificaSenha);
+
+function validaAoEnviar() {
+
+    return (verificaNome() && verificaEmail() && verificaTelefone() && verificaSenha());
+
+}
+
+function verificaNome() {
+
+    // expressão regular para letras (inclusive acentos e cedilha) e espaços
+    // o nome deve ter entre 6 e 100 caracteres
+    const expressaoRegularLetrasEEspacos = /^[a-zA-Z\u00C0-\u00FF ]{6,100}$/;
+
+    return validar(inputNome, expressaoRegularLetrasEEspacos, containerErroNome);
+
+}
+
+
+function verificaEmail() {
+
+    // Expressão regular para email válido
+    const expressaoRegularEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    return validar(inputEmail, expressaoRegularEmail, containerErroEmail);
+
+}
+
+function verificaTelefone() {
+
+    // Expressão regular para telefone válido
+    // o telefone pode ter 8 ou 9 digitos. Além disso, pode ter DDD com ou sem parênteses. 
+    // Exemplos de telefones válidos:
+    // (45) 91231-6165
+    // (89) 3231-6165
+    // 32 91231-6165
+    // 16 3231-6164
+    // 3231-6165
+    // 91231-6165
+    // 33271543
+    // 55997180314
+    const expressaoRegularTelefone = /^(\(?\d{2}\)?\s?)?(\d{4,5}\-?\d{4})$/;
+
+    return validar(inputTelefone, expressaoRegularTelefone, containerErroTelefone);
+
+}
+
+function verificaSenha() {
+
+    // Expressão regular senha
+    // A senha deve ter de 8 a 15 caracteres, pelo menos uma letra minuscula, uma letra maiuscula, um número e um caracter especial
+    const expressaoRegularSenha = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+    return validar(inputSenha, expressaoRegularSenha, containerErroSenha);
+}
+
+// Verifica se o valor do campo input está de acordo com uma expresão regular
+// Se não estiver de acordo, torna visivel o container com a mensagem de erro
+function validar(campoInput, expressaoRegular, containerErro) {
+
+    if (campoInput.value.match(expressaoRegular)) {
+        containerErro.style.display = "none";
+        return true;
+    } else {
+        containerErro.style.display = "block";
+        campoInput.focus();
+        return false;
     }
-    function verificaEmail(){
-        
-        if (email.value.length >= 8) {
-            email.style.border = "1px solid silver";
-            erro_email.style.display = "none";
-            return true;
-        }
-    }
-    function verificaSenha(){
-        if (senha.value.length >= 5) {
-            senha.style.border = "1px solid silver";
-            erro_senha.style.display = "none";
-            return true;
-        }
-    }
-    
-    
+
+
+}
+
